673df949040125cbd9460a9778867179dd12037d
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-04-20 04:47:34 -0400

    completed all problems including stretch

--- index.js
+++ index.js
@@ -2,24 +2,23 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+	return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 // 👇 COMPLETE YOUR WORK BELOW 👇
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -30,49 +29,49 @@ function processFirstItem(stringList, callback) {
 
 /**
  * ### Challenge `processLength`
- * 
+ *
  * @instructions
  * PLEASE STUDY THE EXAMPLE CHALLENGE THOROUGHLY BEFORE PROCEEDING!
  * PLEASE STUDY THE EXAMPLE CHALLENGE THOROUGHLY BEFORE PROCEEDING!
- * 
+ *
  * Implement a higher-order function called `processLength`.
  * It takes two arguments:
  * @param list an array with elements of any type.
  * @param callback function that takes a number as its argument.
  * @returns the result of invoking `callback` passing the LENGTH of `list`.
- * 
+ *
  * Examples of usage of this higher-order function:
  * [1] Invoking `processLength` passing `['foo', 'bar']` and `(num) => num + 1000`,
  * should return 1002.
- * 
+ *
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
-*/
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processLength(list, callback) {
+	return callback(list.length);
 }
 
 /**
  * ### Challenge `processLastItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processLastItem`.
  * It takes two arguments:
  * @param stringList array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the LAST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
-*/
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processLastItem(stringList, callback) {
+	return callback(stringList[stringList.length - 1]);
 }
 
 /**
  * ### Challenge `processSum`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processSum`.
  * It takes three arguments:
@@ -80,21 +79,21 @@ function processLastItem(/* CODE HERE */) {
  * @param num2 a number.
  * @param callback function that takes a number as its argument.
  * @returns the result of invoking `callback` passing the SUM of `num1` and `num2`.
- * 
+ *
  * Examples of usage of this higher-order function:
  * [1] Invoking `processSum` passing `10`, `30` and `(num) => num + " is a big number!"`,
  * should return "40 is a big number!".
- * 
+ *
  * [2] Invoking `processSum` passing `-5`, '-1', and `(num) => num + 1000`,
  * should return 994.
-*/
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processSum(num1, num2, callback) {
+	return callback(num1 + num2);
 }
 
 /**
  * ### Challenge `processProduct`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processProduct`.
  * It takes three arguments:
@@ -102,119 +101,123 @@ function processSum(/* CODE HERE */) {
  * @param num2 a number.
  * @param callback function that takes a number as its argument.
  * @returns the result of invoking `callback` passing the PRODUCT of `num1` and `num2`.
- * 
+ *
  * Examples of usage of this higher-order function:
  * [1] Invoking `processProduct` passing 2 and 7 and `(num) => num + " is a big number!"`,
  * should return "14 is a big number!".
- * 
+ *
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
-*/
-function processProduct(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function processProduct(num1, num2, callback) {
+	return callback(num1 * num2);
 }
 
 /**
  * ### Challenge `processDuplicateFree`
  * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processDuplicateFree`.
  * It takes two arguments:
  * @param list array of elements of any kind.
  * @param callback function that takes an array as its argument.
  * @returns the result of invoking `callback` passing a de-duped version of `list`.
- * 
+ *
  * Examples of usage of this higher-order function:
  * [1] Invoking `processDuplicateFree` passing `[1,1,1,2]` and `(arr) => arr`,
  * should return `[1,2]`.
- * 
+ *
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
-*/
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+ */
+function processDuplicateFree(list, callback) {
+	const noDupes = list.filter((a, b) => list.indexOf(a) === b);
+	return callback(noDupes);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
 
-
 /**
  * ### Challenge `lowerCaseStrings`
- * 
+ *
  * @instructions
  * Implement this function using forEach().
- * 
+ *
  * @param strings an array of strings.
  * @returns an array of equal length to `strings` containing lowercased versions of each string.
- * 
- * 
+ *
+ *
  * Examples of usage of this function:
  * [1] Invoking `lowerCaseStrings` with `[ 'Orange', 'APPLE', 'banana', 'mAnGo']` will return `[ 'orange', 'apple', 'banana', 'mango' ]`.
- * 
+ *
  * [2] Invoking `lowerCaseStrings` with `['a', 'b', 'c' ]` will return `[ 'a', 'b', 'c' ]`.
-*/
-function lowerCaseStrings(/* code here */) {
-  /* code here */
+ */
+function lowerCaseStrings(strings) {
+	let lowerCase = [];
+	strings.forEach((string) => {
+		lowerCase.push(string.toLowerCase());
+	});
+	return lowerCase;
 }
 
 /**
  * ### Challenge `isItAnApple`
- * 
+ *
  * @instructions
  * Implement this function using map().
- * 
+ *
  * @param strings an array of strings.
  * @returns an array of equal length to `strings` containing `true` if the corresponding entry in the `strings` is 'apple' and `false` if it is anything else.
- * 
- * 
+ *
+ *
  * Examples of usage of this function:
  * [1] Invoking `isItAnApple` with `[ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ]` will return `[ false, true, false, false, true, false ]`.
- * 
+ *
  * [2] Invoking `isItAnApple` with `['a', 'b', 'c' ]` will return `[ false, false, false ]`.
-*/
-function isItAnApple(/* code here */) {
-  /* code here */
+ */
+function isItAnApple(strings) {
+	return strings.map((string) => string === 'apple');
 }
 
 /**
  * ### Challenge `removeApple`
- * 
+ *
  * @instructions
  * Implement this function using filter().
- * 
+ *
  * @param strings an array of strings.
  * @returns a similar array, with an entries that are 'apple' removed.
- * 
+ *
  *This function is case sensitive and, for example, should not remove 'Apple' or 'APPLE'
- * 
+ *
  * Examples of usage of this function:
  * [1] Invoking `removeApple` with `[ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ]` will return `[ 'orange', 'banana', 'apples', 'mango' ]`.
- * 
+ *
  * [2] Invoking `removeApple` with `['a', 'b', 'c' ]` will return `[ 'a', 'b', 'c' ]`.
-*/
-function removeApple(/* code here */) {
-  /* code here */
+ */
+function removeApple(strings) {
+	return strings.filter((string) => string !== 'apple');
 }
 
 /**
  * ### Challenge `stringSmash`
- * 
+ *
  * @instructions
  * Implement this function using reduce(). Do NOT use any other array methods.
- * 
+ *
  * @param strings an array of strings.
  * @returns a string with all entries in `strings` combined together.
- * 
- * 
+ *
+ *
  * Examples of usage of this function:
  * [1] Invoking `stringSmash` with `[ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ]` will return 'orangeapplebananaapplesapplemango'.
- * 
+ *
  * [2] Invoking `stringSmash` with `['a', 'b', 'c' ]` will return `abc`.
-*/
-function stringSmash(/* code here */) {
-  /* code here */
+ */
+function stringSmash(strings) {
+	return strings.reduce((acc, cur) => acc + cur);
 }
 
 // A local community center is holding a fund raising 5k fun run and has invited
@@ -224,65 +227,65 @@ function stringSmash(/* code here */) {
 
 /**
  * ### Challenge `getFullNames`
- * 
+ *
  * @instructions
  * Implement this function using forEach() or map().
- * 
+ *
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
-*/
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function getFullNames(runners) {
+	return runners.map((runner) => `${runner.last_name}, ${runner.first_name}`);
 }
 
 /**
  * ### Challenge `firstNamesAllCaps`
- * 
+ *
  * @instructions
- * The event director needs to have all the runners' first names 
+ * The event director needs to have all the runners' first names
  * in uppercase because the director BECAME DRUNK WITH POWER.
  * Implement this function using map().
- * 
+ *
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
-*/
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function firstNamesAllCaps(runners) {
+	return runners.map((runner) => runner.first_name.toUpperCase());
 }
 
 /**
  * ### Challenge `getRunnersByTShirtSize`
  * * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
- * 
+ *
  * @instructions
  * The event director needs a way to find the runners that need
  * a specific t-shirt size, so they can place the orders easily.
  * Implement this function using filter().
- * 
+ *
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @param tShirtSize string (possible values are "S", "M", "L", "XL", "2XL", "3XL").
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
-*/
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+	return runners.filter((runner) => runner.shirt_size === tShirtSize);
 }
 
 /**
  * ### Challenge `tallyUpDonations`
  *  * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
- * 
+ *
  * @instructions
  * The donations need to be tallied up and reported for tax purposes.
  * Implement this function using reduce().
- * 
+ *
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
-*/
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+function tallyUpDonations(runners) {
+	return runners.reduce((a, c) => a + c.donation, 0);
 }
 
 /////////////// CLOSURES ///////////////
@@ -290,24 +293,26 @@ function tallyUpDonations(/* CODE HERE */) {
 
 /**
  * ### Challenge `counterMaker`
- * 
+ *
  * @instructions
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ * counter1 is a variable containing a function expression created by calling the counterMaker function. it returns undefined because the inner function inside counterMaker doesn't have a return value (which I believe is probably a mistake in the original code).  counter2 is a function declaration created separately. it returns a count up (0, 1, 2, 3, etc.)
+
+ * 2. Which of the two uses a closure? How can you tell?
+ * They are both closures, because both functions are able to access  variables outside of their inner scope (counter1 is accessing the count variable in the outer scope of the countMaker function that was called to create it, and counter2 is accessing the count variable located in the global scope). Technically, all functions in javascript are closures.
+ *
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ * counter1 would be more useful in scenarios where you want to limit access to the counter variable to only the counterMaker function, and counter2 would be useful if you needed to access the count variable from another function.
+ */
 
 // counter1 code
 function counterMaker() {
-  let count = 0;
-  return function counter() {
-    count++;
-  }
+	let count = 0;
+	return function counter() {
+		count++; // should be return count++ ???
+	};
 }
 
 const counter1 = counterMaker();
@@ -316,18 +321,18 @@ const counter1 = counterMaker();
 let count = 0;
 
 function counter2() {
-  return count++;
+	return count++;
 }
 
 /**
  * ### Challenge `counterMakerWithLimit`
  * THIS IS A STRETCH PROBLEM! ATTEMPT ONLY AFTER COMPLETING ALL NON-STRETCH CHALLENGES!
- * 
+ *
  * @instructions
  * Implement a counter maker that takes a max value for the count.
  * A counter created with it will reset itself after reaching the max value.
  * Usage is as follows:
- * 
+ *
  * const counter = counterMakerWithLimit(3)
  * counter() // should return 0
  * counter() // should return 1
@@ -339,9 +344,16 @@ function counter2() {
  * counter() // should return 3
  * counter() // should return 0
  * etc
-*/
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+ */
+
+function counterMakerWithLimit(limit) {
+	let count = 0;
+	return function counter() {
+		if (count > limit) {
+			count = 0;
+		}
+		return count++;
+	};
 }
 
 /////////////// END OF CHALLENGE ///////////////
@@ -349,22 +361,52 @@ function counterMakerWithLimit(/* CODE HERE */) {
 /////////////// END OF CHALLENGE ///////////////
 
 if (typeof exports !== 'undefined') {
-  // IGNORE: Test/Env Detected
-  // For Node/Non-browser test env
-  module.exports = module.exports || {}
-  if (processFirstItem) { module.exports.processFirstItem = processFirstItem }
-  if (processLength) { module.exports.processLength = processLength }
-  if (processLastItem) { module.exports.processLastItem = processLastItem }
-  if (processSum) { module.exports.processSum = processSum }
-  if (processProduct) { module.exports.processProduct = processProduct }
-  if (processDuplicateFree) { module.exports.processDuplicateFree = processDuplicateFree }
-  if (lowerCaseStrings ) { module.exports.lowerCaseStrings = lowerCaseStrings}
-  if (isItAnApple) { module.exports.isItAnApple = isItAnApple }
-  if (removeApple) { module.exports.removeApple = removeApple }
-  if (stringSmash) { module.exports.stringSmash = stringSmash }
-  if (getFullNames) { module.exports.getFullNames = getFullNames }
-  if (firstNamesAllCaps) { module.exports.firstNamesAllCaps = firstNamesAllCaps }
-  if (getRunnersByTShirtSize) { module.exports.getRunnersByTShirtSize = getRunnersByTShirtSize }
-  if (tallyUpDonations) { module.exports.tallyUpDonations = tallyUpDonations }
-  if (counterMakerWithLimit) { module.exports.counterMakerWithLimit = counterMakerWithLimit }
+	// IGNORE: Test/Env Detected
+	// For Node/Non-browser test env
+	module.exports = module.exports || {};
+	if (processFirstItem) {
+		module.exports.processFirstItem = processFirstItem;
+	}
+	if (processLength) {
+		module.exports.processLength = processLength;
+	}
+	if (processLastItem) {
+		module.exports.processLastItem = processLastItem;
+	}
+	if (processSum) {
+		module.exports.processSum = processSum;
+	}
+	if (processProduct) {
+		module.exports.processProduct = processProduct;
+	}
+	if (processDuplicateFree) {
+		module.exports.processDuplicateFree = processDuplicateFree;
+	}
+	if (lowerCaseStrings) {
+		module.exports.lowerCaseStrings = lowerCaseStrings;
+	}
+	if (isItAnApple) {
+		module.exports.isItAnApple = isItAnApple;
+	}
+	if (removeApple) {
+		module.exports.removeApple = removeApple;
+	}
+	if (stringSmash) {
+		module.exports.stringSmash = stringSmash;
+	}
+	if (getFullNames) {
+		module.exports.getFullNames = getFullNames;
+	}
+	if (firstNamesAllCaps) {
+		module.exports.firstNamesAllCaps = firstNamesAllCaps;
+	}
+	if (getRunnersByTShirtSize) {
+		module.exports.getRunnersByTShirtSize = getRunnersByTShirtSize;
+	}
+	if (tallyUpDonations) {
+		module.exports.tallyUpDonations = tallyUpDonations;
+	}
+	if (counterMakerWithLimit) {
+		module.exports.counterMakerWithLimit = counterMakerWithLimit;
+	}
 }
